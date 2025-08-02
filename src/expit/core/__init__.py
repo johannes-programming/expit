e373fed32dbdb65c24d392ad548f619fca6d7905
Johannes <johannes-programming@mailfence.com>
2025-08-02 14:50:33 +0200

    Version 1.0.12

--- src/expit/core/__init__.py
+++ src/expit/core/__init__.py
@@ -15,7 +15,7 @@ def function(x: float) -> float:
     return 1 / (1 + p)
 
 
-@preparse.PreParser(posix=False).click()
+@preparse.PreParser().click()
 @click.command(add_help_option=False)
 @click.help_option("-h", "--help")
 @click.version_option(None, "-V", "--version")
